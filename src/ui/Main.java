a258b63dbc6cb88627a0bb30b86cbc02d53d7a6d
Johan794 <johan.ricardo1104@gmail.com>
2021-04-16 17:54:41 -0500

    Bug fixed

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,50 +1,42 @@
 package ui;
 import java.util.Scanner;
 public class Main {
-
     private  static  int [] blocks;
     private  static StringBuilder out;
-    public Main(){
-
-    }
-
     public static void main(String[] args){
         blocks = new int[3];
       Scanner  sc = new Scanner(System.in);
       int problems, discs;
        out = new StringBuilder();
       problems = sc.nextInt();
-      //permitir varias entradas
+
       while (problems!=0){
         discs = sc.nextInt();
         blocks[0] = discs;
-        out.append(blocks[0]).append(" ").append(blocks[1]).append(" ").append(blocks[2]).append(" ").append("\n");
+        out.append(blocks[0]).append(" ").append(blocks[1]).append(" ").append(blocks[2]).append("\n");
         hanoiSolve(discs,0,2,1);
         out.append("\n");
-        blocks[0]=0;
+        blocks[0]= 0;
         blocks[1] =0;
         blocks[2] =0;
         problems--;
       }
-      System.out.println(out);
+      System.out.print(out);
       sc.close();
 
     }
 
     public  static void hanoiSolve(int discs , int source , int destiny , int aux){
-        //System.out.println(Arrays.toString(blocks)+"estado recurcion");
         if(discs == 1){
             move(destiny,source);
-            out.append(blocks[0]).append(" ").append(blocks[1]).append(" ").append(blocks[2]).append(" ").append("\n");
+            out.append(blocks[0]).append(" ").append(blocks[1]).append(" ").append(blocks[2]).append("\n");
         }else{
-            //System.out.println(Arrays.toString(blocks)+" Estado antes del cambio");
             hanoiSolve(discs-1,source,aux,destiny);
             move(destiny,source);
-            out.append(blocks[0]).append(" ").append(blocks[1]).append(" ").append(blocks[2]).append(" ").append("\n");
+            out.append(blocks[0]).append(" ").append(blocks[1]).append(" ").append(blocks[2]).append("\n");
             hanoiSolve(discs-1,aux,destiny,source);
-            //System.out.println(Arrays.toString(blocks)+" de auxiliar a destino");
         }
-        //System.out.println("\n");
+
     }
 
     public static void move(int destiny , int source){
